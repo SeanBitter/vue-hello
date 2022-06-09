ec76dafb3f1184a3500168a50a2800644fdc6ca5
SeanBitter <paolodig@hotmail.it>
2022-06-09 13:59:34 +0200

    Complete the exercise and the bonus

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*
+
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+*/
+
+const app = new Vue(
+    {
+        el: "#app",
+
+        data: {
+            message: "Welcome to",
+            image: "img/vuejs-ar21.png",
+        }
+    }
+);
+
